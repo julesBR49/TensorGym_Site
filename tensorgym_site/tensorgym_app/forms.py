4fabf4af3d2bd49a005b1a78b7eb4c073641d142
jules <jbrucero@uwo.ca>
2019-09-21 15:06:18 -0400

    add an output veiw

--- tensorgym_site/tensorgym_app/forms.py
+++ tensorgym_site/tensorgym_app/forms.py
@@ -2,10 +2,15 @@ from django import forms
 from Tensors import Equation
 
 class EquationForm(forms.Form):
-    equation = forms.CharField(label='Equation' initial='Input Equation' required=False)
+    equation = forms.CharField(label='Equation', help_text='Input Equation')
     foil = forms.BooleanField(required=False)
     distribute_partials = forms.BooleanField(required=False)
     foil_no_distribute = forms.BooleanField(required=False)
 
+    output_equation = forms.CharField(required=False, initial="")
+
     def compute(self):
-        initial_equation = Equation(self.equation)
+        # initial_equation = Equation(self.equation)
+        if self.foil:
+            self.output_equation = "foiling"
+            print("foil")
--- tensorgym_site/tensorgym_app/views.py
+++ tensorgym_site/tensorgym_app/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .forms import EquationForm
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'tensorgym/home.html')
+    if request.method == 'POST':
+
+        form = EquationForm(request.POST)
+        if form.is_valid():
+            form.equation = form.cleaned_data['equation']
+            form.foil = form.cleaned_data['foil']
+            form.output_equation = ""
+            form.compute()
+            html = "<html><body> Equation: %s </body></html>" % form.output_equation
+            return HttpResponse(html)
+            # return render(request, 'tensorgym/home.html', {'form': form})
+            # return HttpResponseRedirect('')
+
+    else:
+        form = EquationForm()
+    return render(request, 'tensorgym/home.html', {'form': form})
+
+# def home(request):
+#     return render(request, 'tensorgym/home.html')
 
 def about(request):
     return render(request, 'tensorgym/about.html')
