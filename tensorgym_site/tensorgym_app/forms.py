cb27fd5529566c88ee0f130ae910e5199eed6892
jules <jbrucero@uwo.ca>
2019-09-21 16:03:31 -0400

    finalize initial draft

--- tensorgym_site/tensorgym_app/forms.py
+++ tensorgym_site/tensorgym_app/forms.py
@@ -1,16 +1,153 @@
 from django import forms
-from Tensors import Equation
+from Tensors.Equation import Equation
 
 class EquationForm(forms.Form):
+    # input
     equation = forms.CharField(label='Equation', help_text='Input Equation')
+
+    text_statements = forms.BooleanField(required=False)
+    initial_eq = forms.BooleanField(required=False)
+    symmetric_tensors = forms.CharField(required=False)
+    # foil
     foil = forms.BooleanField(required=False)
     distribute_partials = forms.BooleanField(required=False)
     foil_no_distribute = forms.BooleanField(required=False)
 
+    # multiply
+    contract_both = forms.BooleanField(required=False)
+    contract_etas = forms.BooleanField(required=False)
+    contract_deltas = forms.BooleanField(required=False)
+
+    # factor
+    factor_gcf = forms.BooleanField(required=False)
+    factor_term = forms.BooleanField(required=False)
+    term_to_factor = forms.CharField(required=False)
+
+    # replace
+    replace_indices = forms.BooleanField(required=False)
+    indices_to_replace = forms.CharField(required=False)
+    replacement_indices = forms.CharField(required=False)
+    replace_terms = forms.BooleanField(required=False)
+    term_to_replace = forms.CharField(required=False)
+    replacement_term = forms.CharField(required=False)
+
+    # sort
+    combine_like_terms_num = forms.BooleanField(required=False)
+    combine_like_terms = forms.BooleanField(required=False)
+    sort_each = forms.BooleanField(required=False)
+    sort_terms = forms.BooleanField(required=False)
+
+    # output
     output_equation = forms.CharField(required=False, initial="")
 
     def compute(self):
-        # initial_equation = Equation(self.equation)
-        if self.foil:
-            self.output_equation = "foiling"
-            print("foil")
+        try:
+            base_equation = Equation(self.equation, self.symmetric_tensors)
+            self.output_equation = ""
+            if base_equation.getCov():
+                # self.open_pop("../Kivy/images/cov_warning.png")
+                self.output_equation += "MAY CONTAIN LOGIC ERRORS DUE TO THE USE OF COVARIANT DERIVATIVES: PROCEED WITH CAUTION \n \n"
+            if self.initial_eq:
+                if self.text_statements:
+                    self.output_equation += "Starting with the equation \n"
+                self.output_equation += repr(base_equation)
+                self.output_equation += "\n" + "\n"
+            if self.foil:
+                base_equation.getTree().foil(base_equation.getTree().getRoot())
+                if self.text_statements:
+                    self.output_equation += "multiplying out terms and distributing partial derivatives using the product rule \n"
+                self.output_equation += repr(base_equation)
+                self.output_equation += "\n" + "\n"
+            if self.distribute_partials:
+                base_equation.getTree().distributePs(base_equation.getTree().getRoot())
+                if self.text_statements:
+                    self.output_equation += "using the calculus product rule to distribute partial derivatives \n"
+                self.output_equation += repr(base_equation)
+                self.output_equation += "\n" + "\n"
+            if self.foil_no_distribute:
+                base_equation.getTree().noPfoil(base_equation.getTree().getRoot())
+                if self.text_statements:
+                    self.output_equation += "multiplying out terms not under a partial derivative \n"
+                self.output_equation += repr(base_equation)
+                self.output_equation += "\n" + "\n"
+        # CONTRACT
+            if self.contract_both:
+                base_equation.contract(base_equation.getTree().getRoot())
+                if self.text_statements:
+                    self.output_equation += "contracting etas and deltas "
+                self.output_equation += repr(base_equation)
+                self.output_equation += "\n" + "\n"
+            if self.contract_etas:
+                base_equation.contract(base_equation.getTree().getRoot(), 'eta')
+                if self.text_statements:
+                    self.output_equation += "contracting etas \n"
+                self.output_equation += repr(base_equation)
+                self.output_equation += "\n" + "\n"
+            if self.contract_deltas:
+                base_equation.contract(base_equation.getTree().getRoot(), 'delta')
+                if self.text_statements:
+                    self.output_equation += "contracting deltas \n"
+                self.output_equation += repr(base_equation)
+                self.output_equation += "\n" + "\n"
+        # FACTOR
+            if self.factor_gcf:
+                base_equation.factorGCF(base_equation.getTree().getRoot())
+                if self.text_statements:
+                    self.output_equation += "factoring out the greatest common factor \n"
+                self.output_equation += repr(base_equation)
+                self.output_equation += "\n" + "\n"
+            if self.factor_term:
+                base_equation.factorUserInputTree(base_equation.getTree().getRoot(), self.term_to_factor)
+                if self.text_statements:
+                    self.output_equation += "factoring out $ " + self.term_to_factor + " $ \n"
+                self.output_equation += repr(base_equation)
+                self.output_equation += "\n" + "\n"
+        # REPLACE
+            if self.replace_indices:
+                base_equation.replaceIndices(self.indices_to_replace, self.replacement_indices)
+                if self.text_statements:
+                    self.output_equation += "replacing indices $ " + self.indices_to_replace + " $ with indices $ " + self.replacement_indices_str + " $ \n"
+                self.output_equation += repr(base_equation)
+                self.output_equation += "\n" + "\n"
+
+            if self.replace_terms:
+                base_equation.replaceTerms(self.term_to_replace, self.replacement_term)
+                if self.text_statements:
+                    self.output_equation += "replacing $ " + self.term_to_replace + " $ with $ " + self.replacement_term + " $ \n"
+                self.output_equation += repr(base_equation)
+                self.output_equation += "\n" + "\n"
+        # SORT
+            if self.combine_like_terms_num:
+                base_equation.combineLikeTermsWithoutSymCo(base_equation.getTree().getRoot())
+                if self.text_statements:
+                    self.output_equation += "combining like terms \n"
+                self.output_equation += repr(base_equation)
+                self.output_equation += "\n" + "\n"
+            if self.combine_like_terms:
+                base_equation.combineLikeTerms(base_equation.getTree().getRoot())
+                if self.text_statements:
+                    self.output_equation += "combine like terms differing by any (numerical or symbolic) coefficient \n"
+                self.output_equation += repr(base_equation)
+                self.output_equation += "\n" + "\n"
+
+            if self.sort_each:
+                base_equation.sortEach()
+                if self.text_statements:
+                    self.output_equation += "organizing the tensors in each term from least to greatest number of partials \n"
+                self.output_equation += repr(base_equation)
+                self.output_equation += "\n" + "\n"
+
+            if self.sort_terms:
+                base_equation.sortTerms()
+                if self.text_statements:
+                    self.output_equation += "organizing each term from least to greatest number of partials \n"
+                self.output_equation += repr(base_equation)
+                self.output_equation += "\n" + "\n"
+        except Exception as exceptObj:
+            self.output_equation = str(exceptObj) + "\n\nPlease email jbrucero@uwo.ca for if you think this is a bug"
+            # self.exception_str = str(exceptObj)
+            #tb = sys.exc_info()[-1]
+            #stk = traceback.extract_tb(tb, 1)
+            #fname = stk[0][2]
+            #self.exception_str += " method that produced error: " + fname
+            # self.open_except_pop("../Kivy/images/error_popup.png")
--- tensorgym_site/tensorgym_app/views.py
+++ tensorgym_site/tensorgym_app/views.py
@@ -10,11 +10,45 @@ def home(request):
 
         form = EquationForm(request.POST)
         if form.is_valid():
+            # initial
             form.equation = form.cleaned_data['equation']
+            form.text_statements=form.cleaned_data['text_statements']
+            form.initial_eq=form.cleaned_data['initial_eq']
+            form.symmetric_tensors = form.cleaned_data['symmetric_tensors']
+            # multiply
             form.foil = form.cleaned_data['foil']
+            form.foil_no_distribute=form.cleaned_data['foil_no_distribute']
+            form.distribute_partials=form.cleaned_data['distribute_partials']
+
+            # contract
+            form.contract_both=form.cleaned_data['contract_both']
+            form.contract_deltas=form.cleaned_data['contract_deltas']
+            form.contract_etas=form.cleaned_data['contract_etas']
+
+            # factor
+            form.factor_gcf=form.cleaned_data['factor_gcf']
+            form.factor_term=form.cleaned_data['factor_term']
+            form.term_to_factor=form.cleaned_data['term_to_factor']
+
+            # replace
+            form.replace_indices=form.cleaned_data['replace_indices']
+            form.replacement_indices=form.cleaned_data['replacement_indices']
+            form.indices_to_replace=form.cleaned_data['indices_to_replace']
+
+            form.replace_terms=form.cleaned_data['replace_terms']
+            form.term_to_replace=form.cleaned_data['term_to_replace']
+            form.replacement_term=form.cleaned_data['replacement_term']
+
+            # sort
+            form.combine_like_terms=form.cleaned_data['combine_like_terms']
+            form.combine_like_terms_num=form.cleaned_data['combine_like_terms_num']
+            form.sort_each=form.cleaned_data['sort_each']
+            form.sort_terms=form.cleaned_data['sort_terms']
+
             form.output_equation = ""
             form.compute()
-            html = "<html><body> Equation: %s </body></html>" % form.output_equation
+            output = form.output_equation.replace("\n","<br>")
+            html = "<html><body> Equation: %s </body></html>" % output
             return HttpResponse(html)
             # return render(request, 'tensorgym/home.html', {'form': form})
             # return HttpResponseRedirect('')
